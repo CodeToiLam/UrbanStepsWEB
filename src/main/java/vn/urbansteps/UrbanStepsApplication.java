181a94a074caae7057f3ffc90c579d093f409242
Mynewbieprogarm <phamnhusontung2006@gmail.com>
2025-06-11 10:08:13 +0700

    UrbanStepsWEB

---
+++ src/main/java/vn/urbansteps/UrbanStepsApplication.java
@@ -0,0 +1,13 @@
+package vn.urbansteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanStepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/urbansteps/UrbanStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.urbansteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
