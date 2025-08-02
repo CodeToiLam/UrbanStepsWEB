9bbc4679d7ece348185d4e0b97f1431dba6f9768
Lam <lamnd1501@gmail.com>
2025-08-03 01:08:57 +0700

    Revert "Nâng cấp thống kê, thêm đơn hàng và quản lý đơn hàng"
    This reverts commit e96f896ec6c799ed93258d38fbcfbe001b4e1a80.

--- src/main/java/vn/urbansteps/service/ThongKeService.java
+++ src/main/java/vn/urbansteps/service/ThongKeService.java
@@ -13,28 +13,20 @@ public class ThongKeService {
 
     public Map<String, Object> thongKeTongQuan() {
         Map<String, Object> result = new HashMap<>();
-        
-        // Tổng số loại sản phẩm
+        // Tổng số sản phẩm
         Query q1 = entityManager.createNativeQuery("SELECT COUNT(*) FROM SanPham");
-        Object tongSanPham = q1.getSingleResult();
-        result.put("tongSanPham", tongSanPham != null ? ((Number)tongSanPham).intValue() : 0);
-        
+        result.put("tongSanPham", ((Number)q1.getSingleResult()).intValue());
         // Tổng số đơn hàng
         Query q2 = entityManager.createNativeQuery("SELECT COUNT(*) FROM HoaDon");
         result.put("tongDonHang", ((Number)q2.getSingleResult()).intValue());
-        
-        // Tổng doanh thu (chỉ đơn hàng đã hoàn thành)
-        Query q3 = entityManager.createNativeQuery("SELECT SUM(tong_thanh_toan) FROM HoaDon WHERE trang_thai='DELIVERED'");
-        Object doanhThu = q3.getSingleResult();
-        result.put("tongDoanhThu", doanhThu != null ? ((Number)doanhThu).doubleValue() : 0.0);
-        
+        // Tổng doanh thu
+        Query q3 = entityManager.createNativeQuery("SELECT SUM(tong_thanh_toan) FROM HoaDon WHERE trang_thai=3");
+        result.put("tongDoanhThu", ((Number)Optional.ofNullable(q3.getSingleResult()).orElse(0)).doubleValue());
         // Số lượng khách hàng
         Query q4 = entityManager.createNativeQuery("SELECT COUNT(*) FROM KhachHang");
         result.put("tongKhachHang", ((Number)q4.getSingleResult()).intValue());
-        
         // Top 5 sản phẩm bán chạy
         Query q5 = entityManager.createNativeQuery("SELECT TOP 5 sp.ten_san_pham, SUM(hdct.so_luong) AS so_luong_ban FROM HoaDonChiTiet hdct JOIN SanPhamChiTiet spct ON hdct.id_san_pham_chi_tiet=spct.id JOIN SanPham sp ON spct.id_san_pham=sp.id GROUP BY sp.ten_san_pham ORDER BY so_luong_ban DESC");
-        @SuppressWarnings("unchecked")
         List<Object[]> topSP = q5.getResultList();
         List<Map<String, Object>> topSanPham = new ArrayList<>();
         for(Object[] row : topSP) {
@@ -44,7 +36,6 @@ public class ThongKeService {
             topSanPham.add(sp);
         }
         result.put("topSanPhamBanChay", topSanPham);
-        
         return result;
     }
 }
