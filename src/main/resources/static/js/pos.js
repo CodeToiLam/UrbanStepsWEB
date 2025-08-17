57edc94e6b8d957e1c81432cd1ce327eff9ee36d
sang205 <sanghoang12012005@gmail.com>
2025-08-17 21:43:04 +0700

    update đa ngôn ngữ phía admin

--- src/main/resources/static/js/pos.js
+++ src/main/resources/static/js/pos.js
@@ -15,12 +15,12 @@ function liveSearchProducts() {
                 dropdown.innerHTML = '';
                 return;
             }
-                        const f = new Intl.NumberFormat('vi-VN');
-                        dropdown.innerHTML = data.map(sp => {
-                                const img = sp.image || '/images/no-image.svg';
-                                const brand = sp.brand ? `<div class="sug-sub">${sp.brand}</div>` : '';
-                                return `
-                                <div class='search-item' onclick='selectSearchProduct(${sp.id}, "${sp.tenSanPham.replace(/"/g,'&quot;')}", ${sp.giaBan || 0})'>
+            const f = new Intl.NumberFormat('vi-VN');
+            dropdown.innerHTML = data.map(sp => {
+                const img = sp.image || '/images/no-image.svg';
+                const brand = sp.brand ? `<div class="sug-sub">${sp.brand}</div>` : '';
+                return `
+                                <div class='search-item' onclick='selectSearchProduct(${sp.id}, "${sp.tenSanPham.replace(/"/g, '&quot;')}", ${sp.giaBan || 0})'>
                                     <img class="sug-img" src="${img}" alt="" onerror="this.src='/images/no-image.svg'"/>
                                     <div class="sug-meta">
                                         <div class="sug-name">${sp.tenSanPham}</div>
@@ -28,28 +28,29 @@ function liveSearchProducts() {
                                         <div class="sug-price">${f.format(sp.giaBan || 0)} đ</div>
                                     </div>
                                 </div>`;
-                        }).join('');
+            }).join('');
             dropdown.style.display = 'block';
         });
 }
 
 function selectSearchProduct(variantId, name, price) {
-        document.getElementById('search').value = name;
-        document.getElementById('search-dropdown').style.display = 'none';
-        const resultsDiv = document.getElementById('search-results');
-        resultsDiv.innerHTML = `<div class="pos-result-item">
+    document.getElementById('search').value = name;
+    document.getElementById('search-dropdown').style.display = 'none';
+    const resultsDiv = document.getElementById('search-results');
+    resultsDiv.innerHTML = `<div class="pos-result-item">
                 <div class="sug-meta">
                     <div class="sug-name">${name}</div>
-                    <div class="sug-price">${new Intl.NumberFormat('vi-VN').format(price||0)} đ</div>
+                    <div class="sug-price">${new Intl.NumberFormat('vi-VN').format(price || 0)} đ</div>
                 </div>
                 <div class="sug-action">
                     <input type='number' min='1' value='1' id='qty_${variantId}' style='width:70px;'>
-                    <button class='pos-btn' onclick='addToCart(${variantId}, "${name.replace(/"/g,'&quot;')}", ${price||0})'>Thêm</button>
+                    <button class='pos-btn' onclick='addToCart(${sp.id}, "${sp.tenSanPham.replace(/"/g, '&quot;')}", ${sp.giaBan || 0})'>${i18n.addToCart}</button>
                 </div>
             </div>`;
 }
+
 // Đóng dropdown khi click ngoài
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const dropdown = document.getElementById('search-dropdown');
     if (dropdown && !dropdown.contains(e.target) && e.target.id !== 'search') {
         dropdown.style.display = 'none';
@@ -67,27 +68,32 @@ function searchProducts() {
             const resultsDiv = document.getElementById('search-results');
             resultsDiv.innerHTML = '';
             if (data.length === 0) {
-                resultsDiv.innerHTML = '<em>Không tìm thấy sản phẩm.</em>';
+                resultsDiv.innerHTML = `<em>${document.getElementById("i18n-notFound").innerText}</em>`;
                 return;
             }
-                        const f = new Intl.NumberFormat('vi-VN');
-                        data.forEach(sp => {
-                                const div = document.createElement('div');
-                                div.className = 'pos-result-item';
-                                const img = sp.image || '/images/no-image.svg';
-                                div.innerHTML = `
-                                        <img class="sug-img" src="${img}" alt="" onerror="this.src='/images/no-image.svg'"/>
-                                        <div class="sug-meta">
-                                            <div class="sug-name">${sp.tenSanPham}</div>
-                                            ${sp.brand ? `<div class='sug-sub'>${sp.brand}</div>` : ''}
-                                            <div class="sug-price">${f.format(sp.giaBan||0)} đ</div>
-                                        </div>
-                                        <div class="sug-action">
-                                            <input type='number' min='1' value='1' id='qty_${sp.id}' style='width:70px;'>
-                                            <button class='pos-btn' onclick='addToCart(${sp.id}, "${sp.tenSanPham.replace(/"/g,'&quot;')}", ${sp.giaBan||0})'>Thêm</button>
-                                        </div>`;
-                                resultsDiv.appendChild(div);
-                        });
+            const f = new Intl.NumberFormat('vi-VN');
+            data.forEach(sp => {
+                const div = document.createElement('div');
+                div.className = 'pos-result-item';
+                const img = sp.image || '/images/no-image.svg';
+                const addToCartText = document.getElementById("i18n-addToCart").innerText;
+
+                div.innerHTML = `
+    <img class="sug-img" src="${img}" alt="" onerror="this.src='/images/no-image.svg'"/>
+    <div class="sug-meta">
+        <div class="sug-name">${sp.tenSanPham}</div>
+        ${sp.brand ? `<div class='sug-sub'>${sp.brand}</div>` : ''}
+        <div class="sug-price">${f.format(sp.giaBan || 0)} đ</div>
+    </div>
+    <div class="sug-action">
+        <input type='number' min='1' value='1' id='qty_${sp.id}' style='width:70px;'>
+        <button class='pos-btn' onclick='addToCart(${sp.id}, "${sp.tenSanPham.replace(/"/g, '&quot;')}", ${sp.giaBan || 0})'>
+            ${addToCartText}
+        </button>
+    </div>`;
+
+                resultsDiv.appendChild(div);
+            });
         });
 }
 
@@ -99,7 +105,7 @@ function addToCart(id, tenSanPham, giaBan) {
     if (idx >= 0) {
         cart[idx].soLuong += qty;
     } else {
-        cart.push({ sanPhamChiTietId: id, tenSanPham, giaTaiThoidiem: giaBan, soLuong: qty });
+        cart.push({sanPhamChiTietId: id, tenSanPham, giaTaiThoidiem: giaBan, soLuong: qty});
     }
     renderCart();
 }
@@ -112,14 +118,17 @@ function renderCart() {
         const thanhTien = item.giaTaiThoidiem * item.soLuong;
         total += thanhTien;
         const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${item.tenSanPham}</td>
-        <td>${new Intl.NumberFormat('vi-VN').format(item.giaTaiThoidiem||0)}</td>
+        tr.innerHTML = `<td>${item.tenSanPham}</td>
+        <td>${new Intl.NumberFormat('vi-VN').format(item.giaTaiThoidiem || 0)}</td>
             <td>${item.soLuong}</td>
-        <td>${new Intl.NumberFormat('vi-VN').format(thanhTien||0)}</td>
-            <td><button onclick='removeCart(${i})'>Xóa</button></td>`;
+        <td>${new Intl.NumberFormat('vi-VN').format(thanhTien || 0)}</td>
+            <td><button onclick='removeCart(${i})'>
+    ${document.getElementById("i18n-delete").innerText}
+</button></td>
+`;
         tbody.appendChild(tr);
     });
-    document.getElementById('cart-total').innerText = new Intl.NumberFormat('vi-VN').format(total||0);
+    document.getElementById('cart-total').innerText = new Intl.NumberFormat('vi-VN').format(total || 0);
 }
 
 function removeCart(idx) {
@@ -149,17 +158,17 @@ function submitOrder(e) {
     };
     fetch('/pos/order', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
     })
-    .then(res => res.json())
-    .then(order => {
-        document.getElementById('order-result').innerHTML = `<b>Đã tạo đơn thành công!</b> Mã đơn: ${order.maHoaDon}`;
-        cart = [];
-        renderCart();
-        form.reset();
-    })
-    .catch(() => {
-        document.getElementById('order-result').innerHTML = '<span style="color:red">Lỗi tạo đơn!</span>';
-    });
+        .then(res => res.json())
+        .then(order => {
+            document.getElementById('order-result').innerHTML = `<b>Đã tạo đơn thành công!</b> Mã đơn: ${order.maHoaDon}`;
+            cart = [];
+            renderCart();
+            form.reset();
+        })
+        .catch(() => {
+            document.getElementById('order-result').innerHTML = '<span style="color:red">Lỗi tạo đơn!</span>';
+        });
 }
